e32275d3a6cb64d2033abf5a750665483e3fe75a
I-am-Thorfin <89223429+I-am-Thorfin@users.noreply.github.com>
2022-05-15 16:30:31 +0200

    Création des différents fichiers, début du codage

---
+++ front/js/cart.js
@@ -0,0 +1,9 @@
+// Loader les informations de notre API 
+// Réccupérer le contenu de notre local storage
+
+// Créer les éléments du panier, et afficher les valeurs tirées du localstorage. 
+// Rappel : le prix viendra de notre API directement
+
+// Ajouter les options pour modifier les quantités et supprimer les articles.
+
+// Complétion du formulaire. 
---
+++ front/js/product.js
@@ -0,0 +1,14 @@
+// searchParams.get // pour avoir réccupérer l'ID dans l'URL actuelle. 
+
+//  fetch avec le http://localhost:3000/api/products/" + idProduct // pour charger les infos de l'API et pouvoir ressortir l'ensemble des infos sur le produit par la suite.
+
+// For etc etc pour créer à nouveau les éléments qu'on veut afficher, qu'on rechargera de manière dynamique en fonction de l'API. 
+
+// Fonction pour envoyer dans le local storage, basé sur l'evenement 'click" ou submit
+
+//PS : Penser à ne surtout pas envoyer le prix dans le localstorage. ( Utiliser parse json ??) 
+
+
+
+
+ 
