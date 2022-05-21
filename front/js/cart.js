8eb1afd3db0fb126b82f400cd9a3f60e4f6151a8
I-am-Thorfin <89223429+I-am-Thorfin@users.noreply.github.com>
2022-05-21 12:31:59 +0200

    Début du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,12 @@
-// Loader les informations de notre API 
-// Réccupérer le contenu de notre local storage
+let addProduct = JSON.parse(localStorage.getItem("stockedproducts")); // on recupere ce qu'il y a dans le local storage
+console.log(addProduct);
 
-// Créer les éléments du panier, et afficher les valeurs tirées du localstorage. 
+const getProductsListing = async () => {
+  await fetch(`http://localhost:3000/api/products`) // "Await" pour attendre que les informations nous soient données et utilisation de la méthode fetch pour aller chercher l'API
+      .then(response => response.json()
+          .then(JSON => productsListing = JSON)); // Réccupération des données de l'API et passage en format JSON.    
+      console.log(`Tableau des Produits:`, productsListing); // On sort les infos de la console ( utiles pour bien s'assurer que nos informations de l'API sont bien reçues.)
+};
+getProductsListing ()
 
-// Rappel : le prix viendra de notre API directement ( Trouvez comment afficher le prix du bon produit ! IMPORTANT !)
 
-// Ajouter les options pour modifier les quantités et supprimer les articles.
-
-// Complétion du formulaire. 
-
-// Commande.
--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,28 @@
-// searchParams.get // pour avoir réccupérer l'ID dans l'URL actuelle. 
-
-//  fetch avec le http://localhost:3000/api/products/" + idProduct // pour charger les infos de l'API et pouvoir ressortir l'ensemble des infos sur le produit par la suite.
-
-// For etc etc pour créer à nouveau les éléments qu'on veut afficher, qu'on rechargera de manière dynamique en fonction de l'API. 
+// searchParams.get, pour avoir réccupérer l'ID dans l'URL actuelle. 
 
 let idProduct = new URL(window.location.href).searchParams.get("id"); // on récupére l'id avec les paramétres de l'url
 console.log(`ID de notre produit : ${idProduct}`);
-//Récupération des sélecteurs css et des id du HTML pour après
 
-let itemImage     = document.querySelector(".item__img"); // on récupére le selecteur css pour pouvoir mettre l'image après
+// fonction pour récuperer les données de l'api avec l'id du produit ( URL + idProduct) 
+const getProductById = async() => {
+  await fetch(`http://localhost:3000/api/products/${idProduct}`) // on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id dans un "template litterals"
+  .then((response) => response.json()
+  .then(json => product = json));      
+};
+
+
+//*Puisque l'ensemble de nos ID sont déjà définies dans le HTML, on met l'ensemble de nos ID et de nos selecteurs dans des variables*//
+let itemImage     = document.querySelector(".item__img"); // on réccupère le selecteur .item__img.
 let title       = document.getElementById("title"); // on récupéré l'id title du document HTML
 let price       = document.getElementById("price"); // on récupére l'id price du document HTML
 let description = document.getElementById("description"); // on récupére l'id description du document HTML
 
 
-// fonction pour récuperer les données de l'api avec l'id du produit
-const getProductById = async() => {
-    await fetch(`http://localhost:3000/api/products/${idProduct}`) // on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id dans un "template litterals"
-    .then((response) => response.json()
-    .then(json => product = json));      
-};
+
 
 getProductById(); // On appelle la fonction précédente pour récupérer les données de l'API
 
-// fonction pour lier les élements HTML que l'on va créer avec les données de l'api
+// fonction pour intégrer dynamiquement ce qu'on réccupère de l'API afin de le faire apparaître dynamiquement sur la page.
 const showProduct = async() => { 
     await getProductById(); 
 
@@ -54,8 +53,11 @@ const showProduct = async() => {
           color.innerHTML = product.colors[i];
           colorChoice.appendChild(color);
         }
-        addToBasket(product)        
+
+        
+              
 };
+
 showProduct();
 
 /* FONCTION POUR ENVOYER TOUT CE QU'ON VEUT VERS LE LOCAL STORAGE */
@@ -67,34 +69,55 @@ let colorChoice = document.getElementById("colors"); // On stocke les choix de c
 let pushProductArrayToLocalStorage = JSON.parse(localStorage.getItem("stockedProducts")); // 
 
 
+console.log(pushProductArrayToLocalStorage)
 
 /*Fonction réagissant au clic*/
 
 const addToBasket = () => {
 
-  clickAddToCart.addEventListener("click", ()=>{
-
-     
+  clickAddToCart.addEventListener("click", ()=>{   
 
     if (quantity.value > 0 && quantity.value <=100 && quantity.value != 0 && colors.value != 0) { // On définit la condition. Si la quantité d'article est supérieure à 0 et inférieure ou égale à 100 ET si la couleur n'est pas inexistante ( 0 ) alors... 
   
-      const addColorQuantityandID = Object.assign( {}, product , { // On ajoute de nouvelles propriétés à notre objet "product" via Object.assign
-        addIdProduct: `${idProduct}`, // On ajoute l'ID réccupéré dans product
-        addColors: `${colorChoice.value}`, // On ajoute la couleur choisie dans product
-        addQuantity: `${quantityofProduct.value}`, // On ajoute la quantité souhaitée dans product     
-       });
-      console.log (addColorQuantityandID)
-
-      const addToLocalStorage = () => { // On créé la fonction visant à ajouter des produits dans le localstorage
-        pushProductArrayToLocalStorage.push(addColorQuantityandID); //On utilise la méthode push
-        localStorage.setItem("stockedProducts", JSON.stringify(pushProductArrayToLocalStorage)); // Et on push nos quantités, couleurs et ID avec une clé "stockedProducts".
-      };
+      const addColorQuantityandID = Object.assign( {} ,  //  Via Object.assign, on ajoute a un objet vide les propriétés qu'on veut envoyer dans le LocalStorage ainsi que la totalité des propriétés de l'objet produit
+      {addIdProduct: `${idProduct}`, // On ajoute l'ID réccupéré
+      addColors: `${colorChoice.value}`, // On ajoute la couleur choisie 
+      addQuantity: `${quantityofProduct.value}`}, // On ajoute la quantité souhaitée    
+      );
+    console.log ("test" + addColorQuantityandID)
+    
+    const addToLocalStorage = () => { // On créé la fonction visant à ajouter des produits dans le localstorage
+      pushProductArrayToLocalStorage.push(addColorQuantityandID); //On utilise la méthode push
+      localStorage.setItem("stockedProducts", JSON.stringify(pushProductArrayToLocalStorage)); // Et on push nos quantités, couleurs et ID avec une clé "stockedProducts".
+    };
+    
+        if (pushProductArrayToLocalStorage) { console.log("Si contenu déjà présent dans le storage, contenu ajouté :" + pushProductArrayToLocalStorage)
+        console.log ()
       
-      if (pushProductArrayToLocalStorage) { console.log(pushProductArrayToLocalStorage)}
+      
+      }
 
-      else {pushProductArrayToLocalStorage = [] } // On utilise push, dans notre tableau.
+        else ( pushProductArrayToLocalStorage = [] )
 
-      addToLocalStorage () // On appelle notre fonction pour qu'elle soit opérante;  
+ 
+
+   addToLocalStorage ()
+    
+    
+
+    
+
+//* fonction pour ajouter les éléments identiques au local Storage *//
+
+
+
+    
+
+
+      
+
+      
+
 
       alert("Vos articles ont bien été ajoutés au panier");   
     } 
@@ -110,6 +133,8 @@ const addToBasket = () => {
 
 
 
+
+addToBasket()
 
 
 
