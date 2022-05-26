f65a19f51b3b9040903ec3318035d8100b758a76
I-am-Thorfin <89223429+I-am-Thorfin@users.noreply.github.com>
2022-05-26 13:57:58 +0200

    Ajout du prix ( hors local storage )

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,23 +1,28 @@
 let addedProduct = JSON.parse(localStorage.getItem("stockedProducts")); // on recupere ce qu'il y a dans le local storage
 
 
-const getProductsListing = async () => {
-  await fetch(`http://localhost:3000/api/products`) // "Await" pour attendre que les informations nous soient données et utilisation de la méthode fetch pour aller chercher l'API
-      .then(response => response.json()
-          .then(JSON => productsListing = JSON)); // Réccupération des données de l'API et passage en format JSON.    
-      console.log(`Tableau des Produits:`, productsListing); // On sort les infos de la console ( utiles pour bien s'assurer que nos informations de l'API sont bien reçues.)
-};
-getProductsListing ()
 
-
-const showProductInChart = () => { 
-       
-   for (let i=0; i < addedProduct.length; i++ ) 
-
-   {
-    let cartItems = document.getElementById("cart__items"); // On met dans une variable notre ID cart__items dans laquelle on va par la suitre créer les classes.
+  
+    // fonction pour récuperer les données de l'api avec l'id du produit ( URL + idProduct) 
     
-    /*Creation de l'article */
+
+
+
+  for ( let i=0; i < addedProduct.length; i++ ) 
+
+   {       
+    const getProductById = async() => {
+      await fetch(`http://localhost:3000/api/products/${addedProduct[i].addIdProduct}`) // on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id dans un "template litterals"
+      .then((response) => response.json()
+      .then(json => product = json));  
+      console.log(product) 
+
+      console.log(product.imageUrl)
+
+          
+    let cartItems = document.getElementById("cart__items"); // On met dans une variable notre ID cart__items dans laquelle on va par la suitre créer les classes.
+     
+   /*Creation de l'article */
     let article = document.createElement ("article");  // On crée une variable pour nos articles.
     cartItems.appendChild(article); // On crée notre article comme enfant de notre cart__items précédement déclarée.
     article.setAttribute("data-id", addedProduct[i].addIdProduct); // On modifie l'attribut "data-id" de notre article pour y ajouter l'ID du produit stocké dans le local storage.
@@ -29,8 +34,9 @@ const showProductInChart = () => {
     article.appendChild(divCartItemImg);
     /*Creation de l'image en elle même */ 
     let imageProducts = document.createElement ("img");
-    imageProducts.setAttribute("src", "" );
-    imageProducts.setAttribute("alt", "TEXTE ALT PAR ICI" ) 
+    imageProducts.setAttribute('src', product.imageUrl);
+    imageProducts.setAttribute('alt', product.textAlt )
+    divCartItemImg.appendChild(imageProducts); 
     
     /*Creation de la div qui contient les informations*/
     let divCartItemContent = document.createElement ("div");
@@ -44,7 +50,7 @@ const showProductInChart = () => {
 
     let cartItemContentDescriptionH2 = document.createElement ("h2");
     divcartItemContentDescription.appendChild(cartItemContentDescriptionH2);
-    cartItemContentDescriptionH2.innerHTML = "titre à ajouter ici" ;
+    cartItemContentDescriptionH2.innerHTML = product.name ;
 
     let cartItemContentDescriptionPColor = document.createElement ("p");
     divcartItemContentDescription.appendChild(cartItemContentDescriptionPColor);
@@ -52,7 +58,7 @@ const showProductInChart = () => {
 
     let cartItemContentDescriptionPPrice = document.createElement ("p");
     divcartItemContentDescription.appendChild(cartItemContentDescriptionPPrice);
-    cartItemContentDescriptionPPrice.innerHTML = "prix";
+    cartItemContentDescriptionPPrice.innerHTML = product.price + "Euros";
 
     /*Creation de la div qui contient l'affichage de la quantité et le bouton de suppression*/
     let divCartItemContentSettings = document.createElement ("div");
@@ -72,10 +78,7 @@ const showProductInChart = () => {
     // à suivre : ajout du bouton supprimer
 
     }
+    getProductById(); 
+  };
 
-
-}
-
-showProductInChart()
-
-
+ 
